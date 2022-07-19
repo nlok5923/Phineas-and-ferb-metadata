2dd392e0ec9f3b45f11239512b79c4858d182cfa
nlok5923 <nlokhande5923@gmail.com>
2022-07-19 19:17:13 +0530

    minute update

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 let port = process.env.port || 3001;
@@ -13,24 +11,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', metaDataRouter);
-
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 app.listen(port, () => {
   console.log("listening to port", port);
