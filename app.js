c30003c015d9d1c9643b08233a7b944ecca8139d
nlok5923 <nlokhande5923@gmail.com>
2022-07-19 19:22:22 +0530

    minute update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-let port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 
 var metaDataRouter = require('./routes/metaData');
 
