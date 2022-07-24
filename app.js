2734078fa2cd3099ac2dd9c4d27231b88f2a68b1
Nitanshu Lokhande <56120084+nlok5923@users.noreply.github.com>
2022-07-24 14:07:26 +0530

    updated supplied origin

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://metadata-api-phineas-ferb.herokuapp.com');
+  res.setHeader('Access-Control-Allow-Origin', 'https://phineas-and-ferb-nft-collection.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
